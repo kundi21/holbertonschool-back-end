369d88d791bb6b05a2f573ef8dc851292fb924a3
kundi21 <cundi.machado@gmail.com>
2023-08-21 11:43:20 -0700

    fix name

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,35 +1,41 @@
 #!/usr/bin/python3
 """
-Gather data from an API.
+Task #0. Gather data from an API.
 Script that using jsonplaceholder.typicode.com API,
 for a given employee ID,returns information about
 his/her TODO list progress.
 """
-import sys
 import json
-import urllib
 import requests
+import sys
+import urllib
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         sys.exit(1)
-    url = "https://jsonplaceholder.typicode.com/todos"
+    
     user_id = int(sys.argv[1])
-    response = requests.get(url)
-    todos_data = response.json()
+    todos_url = f"https://jsonplaceholder.typicode.com/todos?userId={user_id}"
+    user_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
+    
+    todos_response = requests.get(todos_url)
+    todos_data = todos_response.json()
+    user_response = requests.get(user_url)
+    user_data = user_response.json()    
     completed_tasks = 0
-    total_tasks = len(todos_data)
+    total_tasks = 0
     count_tasks = 0
 
+    employee_name = user_data.get("name")
     for todo in todos_data:
         if todo["userId"] == user_id:
             count_tasks += 1
             if todo["completed"] is True:
                 completed_tasks += 1
     print("Employee {} is done with tasks({}/{}):"
-          .format(user_id, completed_tasks, count_tasks))
+          .format(employee_name, completed_tasks, count_tasks))
 
     for todo in todos_data:
         if todo["userId"] == user_id and todo["completed"] is True:
             print("\t {}".format(todo["title"]))
-
+    
