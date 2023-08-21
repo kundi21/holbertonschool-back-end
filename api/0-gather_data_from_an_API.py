2053bc32d93f8c747f0d27eb608b6d9b43c00bf6
kundi21 <cundi.machado@gmail.com>
2023-08-21 10:49:11 -0700

    fix

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -11,19 +11,25 @@ import urllib
 import requests
 
 if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        sys.exit(1)
     url = "https://jsonplaceholder.typicode.com/todos"
     user_id = int(sys.argv[1])
     response = requests.get(url)
-    todos = response.json()
+    todos_data = response.json()
     completed_tasks = 0
+    total_tasks = len(todos_data)
     count_tasks = 0
 
-    for todo in todos:
-        if todo.get('userId') == int(user_id):
+    for todo in todos_data:
+        if todo["userId"] == user_id:
             count_tasks += 1
-            if todo.get('completed') is True:
+            if todo["completed"] is True:
                 completed_tasks += 1
-                print("\t {}".format(todo.get('title')))
+    print("Employee {} is done with tasks({}/{}):"
+          .format(user_id, completed_tasks, count_tasks))
 
-    print("Employee {} is done with tasks ({}/{})".format
-          (user_id, completed_tasks, count_tasks))
+    for todo in todos_data:
+        if todo["userId"] == user_id and todo["completed"] is True:
+            print("\t {}".format(todo["title"]))
+
