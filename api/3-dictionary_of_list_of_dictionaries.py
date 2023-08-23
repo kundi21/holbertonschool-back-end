144ab8cef543bf6febd0cb26a2729dc38351c592
kundi21 <cundi.machado@gmail.com>
2023-08-23 06:45:37 -0700

    task 3

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-task #2. Export to Json
+task #3. Dictionary of list of dictionaries
 Using what you did in task #0,
 extend your Python script to export data in the JSON format.
 """
@@ -9,25 +9,32 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        sys.exit(1)
-
-    user_id = int(sys.argv[1])
-    todos_url = f"https://jsonplaceholder.typicode.com/todos?userId={user_id}"
-    user_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
+    todos_url = "https://jsonplaceholder.typicode.com/todos"
+    users_url = "https://jsonplaceholder.typicode.com/users"
 
     todos_response = requests.get(todos_url)
     todos_data = todos_response.json()
-    user_response = requests.get(user_url)
-    user_data = user_response.json()
-    todo_all_employees = {}
-    for todo in todos_data:
-        todo_all_employees[todo.get("userId")] = [{
-            "task": todo.get("title"),
-            "completed": todo.get("completed"),
-            "username": user_data.get("username")
-        }]
-        with open("todo_all_employees.json", "w") as f:
-            json.dump(todo_all_employees, f)
-            f.close()
-    
+    users_response = requests.get(users_url)
+    users_data = users_response.json()
+
+    all_employee_tasks = {}
+
+    for user in users_data:
+        user_id = user["id"]
+        username = user["username"]
+        user_tasks = []
+
+        for todo in todos_data:
+            if todo["userId"] == user_id:
+                user_tasks.append({
+                    "task": todo["title"],
+                    "completed": todo["completed"],
+                    "username": username
+                })
+
+        all_employee_tasks[str(user_id)] = user_tasks
+
+    filename = "todo_all_employees.json"
+
+    with open(filename, mode="w", encoding="utf-8") as f:
+        json.dump(all_employee_tasks, f, indent=4)
